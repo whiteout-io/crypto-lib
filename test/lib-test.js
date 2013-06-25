1a43c51ff6aa0946836a18c8dc7421fbc9bb66fa
Tankred Hase <mail@tankredhase.de>
2013-06-25 16:38:30 +0200

    seperated RSA verification and item key decryption from AES item encryption

--- test/lib-test.js
+++ test/lib-test.js
@@ -20,7 +20,33 @@ function doTests(assert, lib) {
 			});
 		});
 
-		describe("En/Decrypt for User", function() {
+		describe("En/Decrypt item for User", function() {
+			it('return decrypt the given plaintext', function() {
+
+				var msg = 'Hello, World!',
+					exported = lib.rsa.exportKeys();
+
+				// package into batchable envelope for encryption
+				var envelope = {
+					id: lib.util.UUID(),
+					plaintext: msg,
+					key: lib.util.random(libTest.aesKeysize),
+					iv: lib.util.random(libTest.aesKeysize),
+					receiverPk: exported._id
+				};
+
+				// encrypt
+				lib.cryptoBatch.encryptItemForUser(envelope, exported.pubkeyPem, exported._id);
+				assert.ok(envelope.ciphertext);
+
+				// decrypt
+				lib.cryptoBatch.decryptItemForUser(envelope, exported.pubkeyPem, exported._id);
+				assert.equal(msg, envelope.plaintext);
+
+			});
+		});
+
+		describe("En/Decrypt list for User", function() {
 			it('return decrypt the given plaintext', function() {
 
 				var msg = 'Hello, World!',
@@ -52,6 +78,43 @@ function doTests(assert, lib) {
 				// decrypt
 				var decryptedList = lib.cryptoBatch.decryptListForUser(encryptedList, [publicKey], privateKey);
 				assert.equal(msg, decryptedList[0]);
+
+			});
+		});
+
+		describe("En/Decrypt keys and items seperately for User", function() {
+			it('return decrypt the given plaintext', function() {
+
+				var msg = 'Hello, World!',
+					exported = lib.rsa.exportKeys(),
+					publicKey = {
+						_id: exported._id,
+						publicKey: exported.pubkeyPem
+					},
+					privateKey = {
+						_id: exported._id,
+						privateKey: exported.privkeyPem
+					};
+
+				// package into batchable envelope for encryption
+				var envelopes = [{
+						id: lib.util.UUID(),
+						plaintext: msg,
+						key: lib.util.random(libTest.aesKeysize),
+						iv: lib.util.random(libTest.aesKeysize),
+						receiverPk: publicKey._id
+					}
+				];
+
+				// encrypt
+				var encryptedList = lib.cryptoBatch.encryptListForUser(envelopes, [publicKey], privateKey);
+				assert.ok(encryptedList);
+				assert.equal(encryptedList.length, 1);
+
+				// decrypt
+				lib.cryptoBatch.decryptListKeysForUser(encryptedList, [publicKey], privateKey);
+				lib.cryptoBatch.decryptList(encryptedList);
+				assert.equal(msg, encryptedList[0].plaintext);
 
 			});
 		});
