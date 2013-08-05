adbf81cf6f4fd03d093fde18dc02c683725f01dd
Tankred Hase <tankred@whiteout.io>
2013-08-05 16:19:15 +0200

    update forge to ~0.1.14 from npm, add copy-deps script

---
+++ res/copy-deps.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "--> copying dependencies to src\n"
+
+# go to root
+cd `dirname $0`
+cd ..
+
+mkdir -p test/lib/
+cp node_modules/node-forge/js/*.js test/lib
+cp node_modules/mocha/mocha.css node_modules/mocha/mocha.js test/lib
+cp node_modules/chai/chai.js test/lib
+cp node_modules/node-uuid/uuid.js test/lib
+cp node_modules/underscore/underscore-min.js test/lib
